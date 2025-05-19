d63a7c325f6b41ea76170b595b3bb7d978928f9d
Jorge Garrido Zarzoso <84621249+JorgeGarrido98@users.noreply.github.com>
2025-05-19 17:21:23 +0200

    Update delete_heavy_files.sh

--- delete_heavy_files.sh
+++ delete_heavy_files.sh
@@ -12,14 +12,20 @@ archivos=$(ls)
 
 for archivo in $archivos
 do
-	if [ -f "$archivo" ]; then
-		espacio=$(du -b "$archivo" | awk '{print $1}')
-		echo "El archivo $archivo ocupa $espacio bytes"
+        if [ -f "$archivo" ]; then
+                espacio=$(du -b "$archivo" | awk '{print $1}')
+                echo "El archivo $archivo ocupa $espacio bytes"
 
-		if [ "$espacio" -gt 1000000 ]; then
-			echo "Elinamos $archivo porque ocupa más de 1MB"
-			rm $archivo
-			echo -e "\n"
-		fi
-	fi
+                if [ "$espacio" -gt 1000000 ]; then
+                        echo "El archivo $archivo ocupa más de 1MB"
+                        read -p "¿Desea eliminarlo? (S/N): " confirmacion
+                        if [ "$confirmacion" = "s" ] || [ "$confirmacion" = "S" ]; then
+                                rm $archivo
+                                echo "Archivo $archivo eliminado."
+                        else
+                                echo "Archivo $archivo conservado."
+                        fi
+                        echo -e "\n"
+                fi
+        fi
 done
